7906b6d8264d49b612acedb80a7d3307d8020478
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-05-13 14:05:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-let swiperPopular = new Swiper(".popular-container", {
-  slidesPerView: 1,
-  spaceBetween: 40,
-  loop: false,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
-
-const tl = gsap.timeline({ delay: 1.75 });
+var tl = gsap.timeline({ delay: 1.75 });
 const movements = [-100, 300, 150, -300, -90, 100, -200];
 gsap.set("section", { opacity: 0, display: "none" });
 
@@ -39,11 +28,34 @@ tl.to(".counter p", {
 
 tl.from(".tagline", { y: 40, opacity: 0 }, "-=4");
 
+// Define the font sizes for large and small devices
+const largeDeviceFontSize = "15vw";
+const smallDeviceFontSize = "20vw"; // Adjust this value as needed
+
+// Check if the device is small
+const isSmallDevice = window.matchMedia("(max-width: 768px)").matches;
+
+// Set the appropriate font size based on the device size
+const targetFontSize = isSmallDevice
+  ? smallDeviceFontSize
+  : largeDeviceFontSize;
+
+// Apply the font size using gsap
 tl.to(".header-hero", {
-  fontSize: "15vw",
+  fontSize: targetFontSize,
   duration: 1,
   ease: "power3.out",
 });
+
+const headerHero = document.getElementById(".header-hero");
+
+function increaseFont() {
+  if (window.innerWidth < 768) {
+    headerHero.style.fontSize = "20vw";
+  } else {
+    headerHero.style.fontSize = "15vw";
+  }
+}
 
 tl.to(".header-item", { clipPath: "none", duration: 0.1 }, "<");
 
@@ -126,3 +138,141 @@ document.addEventListener("DOMContentLoaded", function () {
     menuItem.addEventListener("click", closeMenu);
   });
 });
+
+// to duplicate html logo
+const copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".product-logos").appendChild(copy);
+
+// Car Product Slider
+
+function init() {
+  let current = 0;
+  let directionForward = true;
+  const items = document.querySelectorAll(".car-slider .product-items .item");
+  const nextBtn = document.querySelector(".car-slider .next");
+  const prevBtn = document.querySelector(".car-slider .prev");
+
+  const getCurrentTime = () => {
+    return;
+  };
+  const getCard = (item) => {
+    return item.querySelector(".card");
+  };
+
+  const getTitle = (item) => {
+    return item.querySelector(".product-title span");
+  };
+
+  const setItems = () => {
+    items.forEach((item, index) => {
+      getTitle(item).innerHTML = getTitle(item).textContent.replace(
+        /\S/g,
+        `<span class="letter">$&</span>`
+      );
+      if (index === current) return;
+
+      anime.set(getCard(item), {
+        translateX: "100vw",
+      });
+      anime.set(getTitle(item).querySelectorAll(".letter"), {
+        clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+      });
+    });
+    anime.set(items[current], {
+      translateX: 0,
+      opacity: 1,
+    });
+  };
+
+  const animate = {
+    in(item) {
+      const card = getCard(item);
+      const title = getTitle(item);
+      var tl = anime.timeline({
+        duration: 1000,
+        easing: "easeOutExpo",
+      });
+      tl.add({
+        targets: card,
+        translateX: directionForward ? ["100vw", 0] : ["-100vw", 0],
+        rotate: [40, 0],
+      }).add(
+        {
+          targets: title.querySelectorAll(".letter"),
+          clipPath: [
+            "polygon(0 0, 100% 0, 100% 0, 0 0 )",
+            "polygon(0 0, 100% 0, 100% 100%, 0 100% )",
+          ],
+          translateY: directionForward ? ["2em", 0] : ["-2em", 0],
+          delay: anime.stagger(40),
+        },
+        "-=1000"
+      );
+      return tl;
+    },
+    out(item) {
+      const card = getCard(item);
+      const title = getTitle(item);
+      const tl = anime.timeline({
+        duration: 1000,
+        easing: "cubicBezier(0.86, 0, 0.07, 1))",
+      });
+
+      tl.add({
+        targets: card,
+        translateX: directionForward ? [0, "100vw"] : [0, "-100vw"],
+        rotate: [0, -40],
+      }).add(
+        {
+          targets: title.querySelectorAll(".letter"),
+          clipPath: [
+            "polygon(0 0, 100% 0, 100% 100%, 0 100% )",
+            "polygon(0 0, 100% 100%, 100% 100%, 0 100% )",
+          ],
+          translateY: directionForward ? [0, "-15em"] : [0, "-15em"],
+          delay: anime.stagger(40),
+          // opacity: 0,
+        },
+        "-=1000"
+      );
+      return tl;
+    },
+  };
+  function updateClasses() {
+    items.forEach((item, index) => {
+      if (index === current) {
+        item.classList.add("is-active");
+      } else {
+        item.classList.remove("is-active");
+      }
+    });
+  }
+  function next() {
+    if (!directionForward) {
+      directionForward = !directionForward;
+    }
+    animate.out(items[current]);
+    current = (current + 1) % items.length; // fixed index calculation
+    setTimeout(function () {
+      animate.in(items[current]);
+    }, 500);
+    updateClasses();
+  }
+
+  function prev() {
+    if (directionForward) {
+      directionForward = !directionForward;
+    }
+    animate.out(items[current]);
+    current = (current - 1 + items.length) % items.length; // fixed index calculation
+    setTimeout(function () {
+      animate.in(items[current]);
+    }, 500);
+    updateClasses();
+  }
+  nextBtn.addEventListener("click", next);
+  prevBtn.addEventListener("click", prev);
+  setItems();
+}
+
+document.addEventListener("DOMContentLoaded", init);
